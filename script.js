e060c8930a2e1d68892ac4c681a2b85a0fa1d28e
Mars Brown <meeshajean33@gmail.com>
2023-01-18 19:23:26 -0500

    game over parameters set

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 const dino = document.getElementById("dino");
 
 function jump() {
-    if (dino.classList != "jump") {
-  dino.classList.add("jump");
+  if (dino.classList != "jump") {
+    dino.classList.add("jump");
 
-  setTimeout(function () {
-    dino.classList.remove("jump");
-  }, 300);
-}}
+    setTimeout(function () {
+      dino.classList.remove("jump");
+    }, 300);
+  }
+}
+
+let isAlive = setInterval(function () {
+  //get current dino Y position
+  let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+  //get currenct cactus X position
+  let cactusLeft = parseInt(
+    window.getComputedStyle(cactus).getPropertyValue("left")
+  );
+  //detect collision pt
+  if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140) {
+    alert("Game Over!");
+  }
+}, 10);
 
 document.addEventListener("keydown", function (event) {
   jump();
